7d54811d5a6c2780ee7c47a93746a1c34de2f851
SkyLofi <123541242+SkyLofi@users.noreply.github.com>
2024-01-26 15:00:50 +0000

    first commit

---
+++ quick_sort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace program
+{
+    class MyClass
+    {
+        static void swap (int[] numbars, int pivot)
+        static int quicksort(int[] numbars)
+        {
+            int pivot = numbars.Length;
+            for(int i = 0; )
+        }
+        static void main(string[] args)
+        {
+        int[] numbars = {3, 2, 4, 5, 8, 6, 9, 7 ,0 ,1};
+        
+        quicksort(numbars);
+        }
+    }
+}
